ed6579ceba1285d3b15be2381c975cd0a077ff65
Ryan Cheng <rz.cheng06@gmail.com>
2023-08-28 21:46:14 -0700

    first commit

---
+++ FileHelper.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * FileHelper
+ */
+public class FileHelper {
+
+    public static void writeToFile (String s) throws IOException
+    {
+        
+    }
+
+    public static String readFromFile () throws IOException
+    {
+       
+    }
+}
